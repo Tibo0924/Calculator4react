746445f247cb73388e3f9bc19994703a55b7895d
Tibo Keri <tiborkeri@icloud.com>
2018-03-29 13:25:44 +0100

    basic functionality

--- main.js
+++ main.js
@@ -1,69 +1,69 @@
-/*Write a function called squareNumber that will take one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
-Write a function called halfNumber that will take one argument (a number), divide it by 2, and return the result. It should also log a string like "Half of 5 is 2.5.".
-Write a function called percentOf that will take two numbers, figure out what percent the first number represents of the second number, and return the result. It should also log a string like "2 is 50% of 4."
-Write a function called areaOfCircle that will take one argument (the radius), calculate the area based on that, and return the result. It should also log a string like "The area for a circle with radius 2 is 12.566370614359172."
-Bonus: Round the result so there are only two digits after the decimal.
-Write a function that will take one argument (a number) and perform the following operations, using the functions you wrote earlier1:
-Take half of the number and store the result.
-Square the result of #1 and store that result.
-Calculate the area of a circle with the result of #2 as the radius.
-Calculate what percentage that area is of the squared result (#3).
- */
+$(document).ready(function() {
 
-function numStrore(number) {
-  var half = halfNumber(number);
-  var square = squareNumber(half);
-  var areaofC = areaOfCircle(square);
-  var whatPerc = percentOf(areaofC);
-}
+  // get all the id-s and addEventListener to all of them
+  // [...document.querySelectorAll("button")].forEach(function(index) 
+    //get the display
+  document.getElementById("display");
 
-function squareNumber(num) {
-  console.log(
-    `The result of squaring the number ${num} is ${num * num}`,
-    num * num
-  );
-  return num * num;
-}
+  var testNumLength = function(number) {
+    if (number.length > 9) {
+      totaldiv.text(number.substr(number.length - 9, 9));
+      if (number.length > 15) {
+        number = "";
+        totaldiv.text("Err");
+      }
+    }
+  };   
 
-function halfNumber(num) {
-  consol.log(`half of  ${num} is ${num / 2} `, num / 2);
-}
-function percentOf(A, B) {
-  console.log(`${A} is ${A / B * 100}% of ${B}`);
-  return A / B * 100;
-}
-function areaOfCircle(radius) {
-  console.log(
-    `The area for a circle with radius ${radius}is ${(
-      2 *
-      Math.PI *
-      radius
-    ).toFixed(2)}`
-  );
-  return (2 * Math.PI * radius).toFixed(2);
-}
-
-// get all the id-s
-
-// addEventListener to all of them
-/*
-NodeList.prototype.forEach = Array.prototype.forEach; 
-HTMLCollection.prototype.forEach = Array.prototype.forEach;
-*/
-
-//get the display
-document.getElementById("display");
-
-[...document.querySelectorAll("button")].forEach(function(index) {
-  $(index).click(function(e) {
-    var firedButton = e.target.innerHTML;
-    console.log(firedButton);
-    display.innerHTML += firedButton;
+  var number = "";
+  var newnumber = "";
+  var operator = "";
+  var totaldiv = $("#total");
+	totaldiv.text("0");
+	
+  $("#numbers > button").not("#clear,#clearall").click(function() {
+      number += $(this).text();
+      totaldiv.text(number);
+      testNumLength(number);
+    });
+  $("#operators > button").not("#equals").click(function() {
+      operator = $(this).text();
+      newnumber = number;
+      number = "";
+      totaldiv.text("0");
+    });
+  $("#clear,#clearall").click(function() {
+    number = "";
+    totaldiv.text("0");
+    if ($(this).attr("id") === "clearall") {
+      newnumber = "";
+    }
   });
+
+  $("#equals").click(function() {
+    const a = parseInt(newnumber, 10) || 0;
+    const b = parseInt(number, 10) || a;
+    var n;
+    switch (operator) {
+      case "+":
+        n = a + b;
+        break;
+      case "-":
+        n = a - b;
+        break;
+      case "*":
+        n = a * b;
+        break;
+      case "/":
+        n = a / b || 0;
+        break;
+      default:
+        n = 0;
+    }
+    totaldiv.text(n.toString(10));
+    testNumLength(n);
+    [number, newnumber] = ["", ""];
+	});
 });
 
-// Array.from(querySelectorAll('button')).forEach(function(index){
-// $('button').click(function(e){
-//   console.log('mivaan');
-// }
-// )})
+
